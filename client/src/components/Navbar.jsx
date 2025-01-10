284b219b145c89536b20b74bb079652d629be703
imihirjain <jmanav251@gmail.com>
2025-01-10 23:46:19 +0530

    setup project

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import { assets } from "../assets/assets";
+const Navbar = () => {
+  return (
+    <div>
+      <img src={assets.logo} className="w-28 sm:w-32 lg:w-40" alt="" />
+    </div>
+  );
+};
+
+export default Navbar;
+Navbar;
---
+++ client/src/pages/BuyCredit.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BuyCredit = () => {
+  return <div>Buy Credit</div>;
+};
+
+export default BuyCredit;
