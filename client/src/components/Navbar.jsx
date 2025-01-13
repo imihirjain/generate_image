aef98ae132f5307797ba12b4d5f5874e45b2865c
imihirjain <jmanav251@gmail.com>
2025-01-14 01:07:36 +0530

    complete login, result, credit page and install framer motion

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { assets } from "../assets/assets";
 import { Link, useNavigate } from "react-router-dom";
 import { AppContext } from "../context/AppContext";
 const Navbar = () => {
-  const { user } = useContext(AppContext);
+  const { user, setShowLogin } = useContext(AppContext);
   const navigate = useNavigate();
   return (
     <div className="flex items-center justify-between py-4">
@@ -41,7 +41,10 @@ const Navbar = () => {
             <p onClick={() => navigate("/buy")} className="cursor-pointer">
               Pricing
             </p>
-            <button className="bg-zinc-800 text-white px-7 py-2 sm:px-10 text-sm rounded-full">
+            <button
+              onClick={() => setShowLogin(true)}
+              className="bg-zinc-800 text-white px-7 py-2 sm:px-10 text-sm rounded-full"
+            >
               Login
             </button>
           </div>
