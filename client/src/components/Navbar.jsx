2062b2081d91b331b6f43bf52a9d413d12916b9c
imihirjain <jmanav251@gmail.com>
2025-02-01 21:33:48 +0530

    minor changes done

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,7 +8,10 @@ const Navbar = () => {
   return (
     <div className="flex items-center justify-between py-4">
       <Link to={"/"}>
-        <img src={assets.logo} className="w-28 sm:w-32 lg:w-40" alt="" />
+        {/* <img src={assets.logo} className="w-28 sm:w-32 lg:w-40" alt="" /> */}
+        <h1 className="font-medium hover:text-blue-500 hover:bg-white transition-all duration-150 ease-in text-xl  rounded-lg text-white bg-blue-600 px-4 py-2 sm:text-2xl sm:font-semibold">
+          PicScribe
+        </h1>
       </Link>
       <div>
         {user ? (
