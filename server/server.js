ecb1a0470a60334bf4cbe5b24e8c2db8c70a8b68
imihirjain <jmanav251@gmail.com>
2025-02-28 13:47:11 +0530

    old server changes

--- server/server.js
+++ server/server.js
@@ -8,31 +8,16 @@ import imageRouter from "./routes/imageRoutes.js";
 const PORT = process.env.PORT || 4000;
 const app = express();
 
-// CORS Configuration
-const corsOptions = {
-  origin: "https://picscribe.vercel.app",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-};
-
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors());
+await connectDB();
 
-// Connect to the database and start the server
-connectDB()
-  .then(() => {
-    app.use("/api/user", router);
-    app.use("/api/image", imageRouter);
+app.use("/api/user", router);
+app.use("/api/image", imageRouter);
+app.get("/", (req, res) => {
+  res.send("API Working ");
+});
 
-    app.get("/", (req, res) => {
-      res.send("API Working");
-    });
-
-    app.listen(PORT, () => {
-      console.log("Server listening on port " + PORT);
-    });
-  })
-  .catch((err) => {
-    console.error("Database connection error:", err);
-    process.exit(1);
-  });
+app.listen(PORT, () => {
+  console.log("Server listening on port " + PORT);
+});
